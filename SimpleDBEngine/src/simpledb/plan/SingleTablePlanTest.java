24196b972315920df42b2b5ed47735e417968df9
Dexter Tan <dev@dextertanyj.com>
2022-01-29 15:10:55 +0800

    Update existing tests

--- SimpleDBEngine/src/simpledb/plan/SingleTablePlanTest.java
+++ SimpleDBEngine/src/simpledb/plan/SingleTablePlanTest.java
@@ -1,4 +1,5 @@
 package simpledb.plan;
+
 import simpledb.server.SimpleDB;
 import simpledb.tx.Transaction;
 import simpledb.metadata.MetadataMgr;
@@ -7,21 +8,21 @@ import java.util.*;
 
 public class SingleTablePlanTest {
 	public static void main(String[] args) throws Exception {
-      SimpleDB db = new SimpleDB("studentdb");
-      MetadataMgr mdm = db.mdMgr();
-      Transaction tx = db.newTx();
+		SimpleDB db = new SimpleDB("studentdb");
+		MetadataMgr mdm = db.mdMgr();
+		Transaction tx = db.newTx();
 
-		//the STUDENT node
+		// the STUDENT node
 		Plan p1 = new TablePlan(tx, "student", mdm);
 
 		// the Select node for "major = 10"
-		Term t = new Term(new Expression("majorid"), new Expression(new Constant(10)));
-		Predicate pred = new Predicate(t); 
+		Term t = new Term(Operator.EQ, new Expression("majorid"), new Expression(new Constant(10)));
+		Predicate pred = new Predicate(t);
 		Plan p2 = new SelectPlan(p1, pred);
 
 		// the Select node for "gradyear = 2020"
-		Term t2 = new Term(new Expression("gradyear"), new Expression(new Constant(2020)));
-		Predicate pred2 = new Predicate(t2); 
+		Term t2 = new Term(Operator.EQ, new Expression("gradyear"), new Expression(new Constant(2020)));
+		Predicate pred2 = new Predicate(t2);
 		Plan p3 = new SelectPlan(p2, pred2);
 
 		// the Project node
@@ -29,21 +30,25 @@ public class SingleTablePlanTest {
 		Plan p4 = new ProjectPlan(p3, c);
 
 		// Look at R(p) and B(p) for each plan p.
-		printStats(1, p1); printStats(2, p2); printStats(3, p3); printStats(4, p4);
-		
+		printStats(1, p1);
+		printStats(2, p2);
+		printStats(3, p3);
+		printStats(4, p4);
+
 		// Change p2 to be p2, p3, or p4 to see the other scans in action.
-		// Changing p2 to p4 will throw an exception because SID is not in the projection list.
+		// Changing p2 to p4 will throw an exception because SID is not in the
+		// projection list.
 		Scan s = p2.open();
 		while (s.next())
-			System.out.println(s.getInt("sid") + " " + s.getString("sname")  
-			+ " " + s.getInt("majorid") + " " + s.getInt("gradyear"));
+			System.out.println(s.getInt("sid") + " " + s.getString("sname")
+					+ " " + s.getInt("majorid") + " " + s.getInt("gradyear"));
 		s.close();
 	}
-	
+
 	private static void printStats(int n, Plan p) {
 		System.out.println("Here are the stats for plan p" + n);
 		System.out.println("\tR(p" + n + "): " + p.recordsOutput());
 		System.out.println("\tB(p" + n + "): " + p.blocksAccessed());
-		System.out.println();	
+		System.out.println();
 	}
 }
